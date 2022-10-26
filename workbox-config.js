c4f0756b22051e3304ccd612dee0c24e1d0632f8
Greatness Prodigy <49532326+Grtnxhor@users.noreply.github.com>
2022-10-26 04:42:15 +0100

    manifest

---
+++ workbox-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+	globDirectory: 'C:\xampp\htdocs\jobs\Toilsify',
+	globPatterns: [
+		'**/*.{png,css,js,json,jpg,eot,svg,ttf,woff,woff2,txt,html}'
+	],
+	swDest: 'C:/xampp/htdocs/jobs/Toilsify/sw.js',
+	ignoreURLParametersMatching: [
+		/^utm_/,
+		/^fbclid$/
+	]
+};
